f3c495240f8d2d5e68b47352d302186c3297440e
ishwor <echo.ishwor@gmail.com>
2019-03-17 18:08:12 +0545

    auto log enable

--- emacs-shortcuts/auto-ssh/auto-ssh.sh
+++ emacs-shortcuts/auto-ssh/auto-ssh.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 readonly ENVIRONMENT=$1
+
+declare SHOW_LOGS=$2
+
+readonly SHOW_LOGS=${SHOW_LOGS:-0}
+
 readonly USER="tms"
 readonly PASS_PHRASE="yco@nepse777+"
 
 readonly QA_1="192.168.50.70"
 readonly QA_2="192.168.50.62"
 
+readonly DUMP_LOG='tail -f /opt/wildfly/standalone/log/server.log; bash -l'
+
 declare -A ENVIRONMENTS=( ["QA-1"]="$QA_1" ["QA-2"]="$QA_2")
 
 readonly IP="${ENVIRONMENTS["${ENVIRONMENT}"]}"
 echo -e "Connecting to server ... $IP (${ENVIRONMENT}) \n"
 
-sshpass -p "${PASS_PHRASE}" ssh -o StrictHostKeyChecking=no "${USER}@${IP}"
 
-echo "*** Connection success ***"
-
-
-
-
-
+if [ "$SHOW_LOGS" -eq 1 ]; then
+    sshpass -p "${PASS_PHRASE}" ssh -o StrictHostKeyChecking=no "${USER}@${IP}" "$DUMP_LOG"
+else
+    sshpass -p "${PASS_PHRASE}" ssh -o StrictHostKeyChecking=no "${USER}@${IP}"
+fi
