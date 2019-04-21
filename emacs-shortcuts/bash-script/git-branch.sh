d549d0b5108099150f29ee416c0d0d40431f3c90
ishwor <echo.ishwor@gmail.com>
2019-04-21 17:12:28 +0545

    verify branch details while creating a tag

--- emacs-shortcuts/bash-script/git-branch.sh
+++ emacs-shortcuts/bash-script/git-branch.sh
@@ -1,18 +1,32 @@
 # checks if branch has something pending
 function parse_git_dirty() {
-  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+    git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
 }
 
 # gets the current git branch
 function parse_git_branch() {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
 }
 
 # get last commit hash prepended with @ (i.e. @8a323d0)
 function parse_git_hash() {
-  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+    git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
 }
 
-# DEMO
-GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
-echo ${GIT_BRANCH}
+
+# get last commit hash 
+function parse_commit_message() {
+    git log --format=%B -n 1 $1 
+}
+
+
+#DEMO
+
+# echo -e "[auto] running auto git update ... \n"
+# git update
+# GIT_COMMIT_HASH=$(parse_git_hash)| cut -d '@' -f 2
+
+ echo "$COMMIT_HASH"
+ GIT_BRANCH="$(parse_git_branch)$(parse_git_hash)@$(parse_commit_message "$GIT_COMMIT_HASH")"
+ echo ${GIT_BRANCH}
+
--- emacs-shortcuts/bash-script/git-deployment.sh
+++ emacs-shortcuts/bash-script/git-deployment.sh
@@ -1,101 +1,148 @@
 #!/bin/bash
-# Bash Menu Script Example
+# Automatic git tagging
 
+GRAY='\033[1;90m'
+FINISHED='\033[1;96m'
+BOLD='\033[1;92m'
+NOCOLOR='\033[0m' # No Color
+ERROR='\033[0;31m'
+PS1_COLOR='\033[00;32m'
 
-trap "exit 1" TERM
-export TOP_PID=$$
+function TerminateWithMsg(){
 
-bold=$(tput bold)
-
+    local readonly file_name=$(basename $0)
+    
+    echo -e "\nTerminating script: $file_name ............... ${FINISHED}Goodbye!${PS1_COLOR}" >&2
+    
+    echo -e "\n" >&2
+    
+}
 
 function BranchValidity(){
     
     if [ "$1" != "MASTER" ]; then
 	
-     	echo -e "\nUnable to create a TAG [✕]\n\n[Error] Expected branch: ${bold}MASTER \033[00;32m, Found branch: ${bold}$1\033[00;32m" >&2
+     	echo -e "\nUnable to create a TAG ${ERROR}[✕]${PS1_COLOR}\n\n${ERROR}[error]${PS1_COLOR} Expected branch: ${BOLD}MASTER ${PS1_COLOR}, Found branch: ${BOLD}$1${PS1_COLOR}" >&2
 	
-	echo -e "\n[Do] ${bold}git checkout master \033[00;32mand re-run this script" >&2
+	echo -e "\n${GRAY}[Do] ${BOLD}git checkout master ${PS1_COLOR}and re-run this script" >&2
 	
-     	file_name=$(basename $0)
-	
-     	echo -e "\nTerminating script: $file_name" >&2
-	
-	echo -e "Goodbye!\n" >&2
-	kill -s TERM $TOP_PID
+     	TerminateWithMsg
+
+	echo 1;
+	exit;
     fi
 
+    echo -e "\nOn a branch : ${BOLD}$1${PS1_COLOR} \n" >&2
+
+    echo -e "${GRAY}[git]${PS1_COLOR}: Branch verification success ${BOLD}[✓]${PS1_COLOR}\n" >&2
+    
     echo 0
 }
+
+
+
+
 
 
 function deploy(){
 
     #0 : new , 1: update , 2: hot
 
-    echo -e "\n[Git]: Branch verification success [✓]"
-    
-    local deployment_type=$2
+    local readonly deployment_type=$2
 
-    local last_snapshot=$1;
-    
-    local project=$(basename `git rev-parse --show-toplevel | tr a-z A-Z`)
-    
-    local most_recent_tag=$(git tag --merged=HEAD --list "$project-*" | sort -r -n -k2 | sort -rn -t $'V' -k2,2 | head -n 1)
-    
-    echo -e "\n[Git]: Snapshot: ${bold}$last_snapshot\033[00;32m \n"
-    
+    local readonly last_snapshot=$1;
+
+    local readonly project=$(basename `git rev-parse --show-toplevel | tr a-z A-Z`)
+
+    local readonly most_recent_tag=$(git tag --merged=HEAD --list "$project-*" | sort -r -n -k2 | sort -rn -t $'V' -k2,2 | head -n 1)
+
+    echo -e "${GRAY}[git]${PS1_COLOR} creating a tag on snapshot: ${BOLD}$last_snapshot${PS1_COLOR} ...\n"
+
     local version_pre=$(echo "$most_recent_tag" |awk -F'-V' '{ print $NF }'|  awk -F'.' '{print $1}' | tr -dc '0-9')
-    
+
     local version_post=$(echo "$most_recent_tag" | awk -F'-V' '{ print $NF }'|awk -F'.' '{print $2}'  | cut -d '-' -f 1)
 
-    local current_date=$(date +"%Y-%m-%d")
+    local readonly current_date=$(date +"%Y-%m-%d")
 
     if (( $deployment_type == 0)) ; then
 	((version_pre++));
 	version_post=0
     elif (( $deployment_type == 2 )) ; then
 	((version_post++));
-	
+
     fi
 
-    
+
     local tag_label="$project-V$version_pre.$version_post-$current_date"
-    
+
     local commit_message="V$version_pre.$version_post-$current_date release"
-    
+
     if (( $deployment_type == 1 )) ; then
 	git tag -a "$tag_label" "$last_snapshot" -m "$commit_message" 2> /dev/null
 	if (( $? != 0 )); then
-	    echo -e "[Warning] tag $tag_label already exists. Force updating ...\n"
+	    echo -e "${GRAY}[warning]${PS1_COLOR} tag $tag_label already exists. Force updating ...\n"
 	    git tag -af "$tag_label" "$last_snapshot" -m "$commit_message" 
 	fi
     else
-	git tag -a "$tag_label" "$last_snapshot" -m "$commit_message" 
+	git tag -af "$tag_label" "$last_snapshot" -m "$commit_message" 
 
     fi
 
-    
-    git tag -fa latest  "$last_snapshot" -m "$commit_message"
-    
-    git push --tags origin master --force
-    
-    echo -e "\n[new tag] success: ${bold}$tag_label\033[00;32m\n"
 
-    echo  -e "[Before]: $most_recent_tag\n[After]: $tag_label\n"
-    
+    git tag -fa latest  "$last_snapshot" -m "$commit_message"
+
+    git push --tags origin master --force
+
+    echo -e "\n${GRAY}[new tag]${PS1_COLOR}: ${BOLD}$tag_label${PS1_COLOR}\n"
+
+    echo  -e "${BOLD}[before]${PS1_COLOR} : $most_recent_tag\n${FINISHED}[after]${PS1_COLOR}  : $tag_label\n"
+
 }
 
+function VerificationDetails(){
+
+    local readonly branch=`echo $branch_snapshot | cut -d '@' -f 1`
+    
+    commit_hash=`echo $branch_snapshot | cut -d '@' -f 2`
+    
+    local readonly commit_message=`echo $branch_snapshot | cut -d '@' -f 3`
+
+    echo -e "${GRAY}[verify]${PS1_COLOR}: details verification before we proceed \n"
+
+    echo -e "${BOLD}======================================================${PS1_COLOR}"
+    
+    echo -e "\n${GRAY}branch${PS1_COLOR}   : ${BOLD}$branch${PS1_COLOR} \n"
+
+    echo -e "${GRAY}snapshot${PS1_COLOR} : ${BOLD}$commit_hash${PS1_COLOR}\n"
+
+    echo -e "${GRAY}message${PS1_COLOR}  : ${BOLD}$commit_message${PS1_COLOR} \n"
+
+    echo -e "${BOLD}=======================================================${PS1_COLOR}\n"
+
+}
+
+
+function UserIntraction(){
+
+    while true; do
+	read -p "should we proceed with tag creation??  Y/N    " yn
+	case $yn in
+            [Yy]* ) echo 0;
+		    break;;
+            [Nn]* ) TerminateWithMsg;
+		    echo 1;
+		    break;;
+            * ) echo "Please answer yes or no.";;
+	esac
+    done
+}
 
 function initScript(){
 
     echo -e "\n"
-    
-    local branch_name=`echo $branch_snapshot | cut -d '@' -f 1 | tr a-z A-Z`
-    local commit_hash=`echo $branch_snapshot | cut -d '@' -f 2`
-    
     local PS3='Please enter your choice: '
 
-    local options=("New Deployment" "Update TAG from last commit" "Hot Fix" "Quit")
+    local readonly options=("New Deployment" "Update TAG from last commit" "Hot Fix" "Quit")
     select opt in "${options[@]}"
     do
 	case $opt in
@@ -103,33 +150,32 @@ function initScript(){
 		deploy "$commit_hash" 0 
 		break
 		;;
-	    
-            "Update TAG from last commit")
+
+	    "Update TAG from last commit")
 		deploy  "$commit_hash" 1
 		break
 		;;
-	    
+
 	    "Hot Fix")
 		deploy "$commit_hash" 2
 		break
 		;;
-	    
-            "Quit")
+
+	    "Quit")
 		break
 		;;
-	    
-            *) echo "invalid option $REPLY";;
+
+	    *) echo "invalid option $REPLY";;
 	esac
 
     done
-    
-}
 
+}
 
 
 script_location=$(dirname "$0")
 
-script_location+="$branch_snapshot/git-branch.sh"
+script_location="$script_location/git-branch.sh"
 
 branch_snapshot=`sh "$script_location"`
 
@@ -138,6 +184,14 @@ branch_name=`echo $branch_snapshot | cut -d '@' -f 1 | tr a-z A-Z`
 is_valid_branch=$(BranchValidity "$branch_name")
 
 if (( $is_valid_branch == 0 )); then
-    initScript
+    
+    VerificationDetails
+    
+    should_proceed=$(UserIntraction)
+    
+    if(( $should_proceed == 0 )); then
+	
+	initScript
+	
+    fi
 fi
-
